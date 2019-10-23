3729f70e297db74a417a38001d642375944873ed
Connor Homan <connorhoman14@gmail.com>
2019-10-23 13:28:01 -0700

    Added names and PINs for all users into Login process, which is now functionally checking form against credentials.

--- react-client/src/components/login/login.jsx
+++ react-client/src/components/login/login.jsx
@@ -6,32 +6,63 @@ const Form = styled.form`
   margin: 0px;
   width: 131px;
   font-family: 'Courier New', Courier, monospace;
+  font-size: 15px;
 `;
-const Title = styled.div`
-  font-weight: 900;
+const Button = styled.button`
   font-family: 'Courier New', Courier, monospace;
-  padding-left: 45px;
+  margin-left: 45px;
+  font-size: 26px;
 `;
 
 class LogIn extends React.Component {
     constructor(props) {
       super(props);
       this.state = { 
+        name: '',
+        pin: '',
+      }
+    }
+
+    login() {
+      console.log('Logged In');
+    }
+
+    failedLogin() {
+      window.alert('Incorrect Username or PIN');
+    }
+
+    checkCredentials() {
+      const credentials = {
+        manasseh: '1214',
+        emyr: '1093',
+        fabian: '1592',
+        carl: '6821',
+        jordan: '8261',
+        marcus: '1209',
+        james: '8712',
+        trevor: '8123',
+        joseph: '9018',
+        connor: '4030',
+      }
+      if (credentials[this.state.name] === this.state.pin) {
+        this.login();
+      } else {
+        this.failedLogin();
       }
     }
   
     render () {
       return (
         <div>
-          <Title>LogIn</Title>
           <Form>
             <div>
-              <input font-family='Courier' placeholder='Name'></input>
+              <input onChange={(e) => this.setState({name: e.target.value})} fontFamily='Courier' placeholder='Name'></input>
             </div>
             <div>
-              <input font-family='Courier' placeholder='PIN'></input>
+              <input onChange={(e) => this.setState({pin: e.target.value})} fontFamily='Courier' placeholder='PIN'></input>
             </div>   
           </Form>
+          <Button onClick={() => this.checkCredentials()}>GO</Button>
         </div>
       );
     }
